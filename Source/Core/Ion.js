7f76a4653d7a948c0f56b930de79fcccffeddc09
yujiezhang125 <1369616003@qq.com>
2021-07-12 17:25:39 +0800

    更新token信息

--- Source/Core/Ion.js
+++ Source/Core/Ion.js
@@ -4,7 +4,7 @@ import Resource from "./Resource.js";
 
 var defaultTokenCredit;
 var defaultAccessToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzMDEzM2RiZi0wODc3LTQxYmMtODcyNy1kZmE4MjFiZDJjMGMiLCJpZCI6MzQyNTgsImlhdCI6MTYwMDA3MjgxMH0.R6gDhL-v8OQXjHzOBNNpcUzBBMQZyDjCs8Phd9Tx5jw";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1MDA2NzVhYi05NjljLTQ2ZWQtYWJkZC1jYTg4NzA4YjEzNGMiLCJpZCI6MjU5LCJpYXQiOjE2MjUxNTA1ODh9.hDxkoBgpk13oYPDuKYhkt6XSyii_kUKufDE_GLE38is";
 /**
  * Default settings for accessing the Cesium ion API.
  *
